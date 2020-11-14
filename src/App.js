364b86114a42213eced77f5cfbddc0e1c487560f
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-11-14 19:02:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import Calendar from "react-calendar";
 function App() {
   // compare the from and put them in order
   function compareFrom(a, b) {
-    changes = changes+1
+    changes = changes + 1;
     if (a.from < b.from) {
-      console.log(emails[1].from);
+      // console.log(emails[1].from);
       return -1;
     }
     if (a.from > b.from) {
-      console.log(emails[1].from);
+      // console.log(emails[1].from);
       return 1;
     }
     return 0;
@@ -21,11 +21,11 @@ function App() {
   // compare the to and put them in order
   function compareTo(a, b) {
     if (a.to < b.to) {
-      console.log(emails[1].to);
+      // console.log(emails[1].to);
       return -1;
     }
     if (a.to > b.to) {
-      console.log(emails[1].to);
+      // console.log(emails[1].to);
       return 1;
     }
     return 0;
@@ -33,11 +33,24 @@ function App() {
   // compare the subjects and put them in order
   function compareSubject(a, b) {
     if (a.subject < b.subject) {
-      console.log(emails[1].subject);
+      // console.log(emails[1].subject);
       return -1;
     }
     if (a.subject > b.subject) {
-      console.log(emails[1].subject);
+      // console.log(emails[1].subject);
+      return 1;
+    }
+    return 0;
+  }
+  function compareDate(a, b) {
+    console.log(a.date)
+    const sortedActivities = emails.slice().sort((a, b) => b.date - a.date)
+    if (a.date < b.date) {
+      // console.log(emails[1].subject);
+      return -1;
+    }
+    if (a.date > b.date) {
+      // console.log(emails[1].subject);
       return 1;
     }
     return 0;
@@ -45,69 +58,162 @@ function App() {
 
   // date functions
   // let fakeTime = "Fri Nov 13 2020 09:28:14 GMT+0900 (Japan Standard Time)";
-  function changeTime() {
-    // let array = fakeTime.split(" ");
-    // switch (array[1]) {
-    //   case "Nov":
-    //     array[1] = "11";
-    //     break;
-    //   case "Jan":
-    //     array[1] = "1";
-    //     break;
-    //   default:
-    //     break;
-    // }
-    // array = array.splice(1, 4);
-    // console.log(array);
-    // fakeTime = array.join(" ");
-    // let now = new Date();
-    // console.log(now);
-    // return fakeTime.toString();
-    return "Bob"
+  function calcDate(emailDate) {
+    let dateArr = emailDate.split(" ");
+    switch (dateArr[1]) {
+      case "Jan":
+        dateArr[1] = "01";
+        break;
+      case "Feb":
+        dateArr[1] = "02";
+        break;
+      case "Mar":
+        dateArr[1] = "03";
+        break;
+      case "Apr":
+        dateArr[1] = "04";
+        break;
+      case "May":
+        dateArr[1] = "05";
+        break;
+      case "Jun":
+        dateArr[1] = "06";
+        break;
+      case "Jul":
+        dateArr[1] = "07";
+        break;
+      case "Aug":
+        dateArr[1] = "08";
+        break;
+      case "Sep":
+        dateArr[1] = "09";
+        break;
+      case "Oct":
+        dateArr[1] = "10";
+        break;
+      case "Nov":
+        dateArr[1] = "11";
+        break;
+      case "Dec":
+        dateArr[1] = "12";
+        break;
+      default:
+        return;
+    }
+    // console.log(dateArr[3] + "/" + dateArr[1] + "/" + dateArr[2]);
+    let dateFormatted = new Date(
+      dateArr[3] + "/" + dateArr[1] + "/" + dateArr[2]
+    );
+    let today = new Date();
+    // console.log(
+    //   today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()
+    // );
+    today = new Date(
+      today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate()
+    );
+    // console.log(Math.ceil((today - dateFormatted) / 86400000));
+    let dateDiff = (today - dateFormatted) / 86400000;
+
+    let splitDate = dateFormatted.toString().split(" ");
+
+    return dateDiff > 365
+      ? dateArr[3] + "/" + dateArr[1] + "/" + dateArr[2]
+      : dateDiff > 1
+      ? splitDate[1] + " " + splitDate[2]
+      : dateArr[4].substring(0, dateArr[4].length - 3);
   }
+
   // end of date functions
   // Calender
   const [value, onChange] = useState(new Date());
-  // Emails (With no server)
+  // Emails
   const [emails, setEmails] = useState([
+  ]);
+
+  // Emails Initial State
+  const allEmails = [
     {
       from: "aaa@exmaple.com",
       to: "zzz@example.com",
       subject: "Second Email",
-      date: "Nov 13 2020 08:59:31",
+      date: "Sun Nov 01 2020 00:00:00 GMT+0900 (Japan Standard Time)",
+    },
+    {
+      from: "aaa@exmaple.com",
+      to: "zzz@example.com",
+      subject: "Second Email",
+      date: "Sun Nov 14 2020 02:00:00 GMT+0900 (Japan Standard Time)",
     },
     {
       from: "ddd@exmaple.com",
       to: "eee@example.com",
       subject: "Third Email",
-      date: changeTime(),
+      date: "Sat Nov 07 2020 00:00:00 GMT+0900 (Japan Standard Time)",
     },
     {
       from: "bbb@exmaple.com",
       to: "ccc@example.com",
       subject: "First Email",
-      date: "Nov 12 2020 08:59:31",
+      date: "Wed Aug 08 2018 00:00:00 GMT+0900 (Japan Standard Time)",
     },
-  ]);
+  ];
+  // Set the emails to be the incoming inital state
+  useEffect(() => {
+    setEmails(allEmails);
+  }, []);
   // Display results (amount of emails)
   let resultsLength = emails.length;
   let changes = 0;
   // Search function to look for emails from the dates
   const SearchClick = (e) => {
     e.preventDefault();
-    console.log(value);
+    const placeHolder = [];
+    for (let index = 0; index < allEmails.length; index++) {
+      const WholeEmail = allEmails[index];
+      const element = allEmails[index].date;
+      console.log(value, element);
+      if (
+        element.split(" ").splice(0, 4).join(" ") ===
+        value.toString().split(" ").splice(0, 4).join(" ")
+      ) {
+        console.log("Match!");
+        placeHolder.push(WholeEmail);
+      } else {
+        console.log("No Match");
+      }
+    }
+    console.log(allEmails);
+    setEmails(placeHolder);
   };
 
-  const emailArrReturn = (emails) => {
+  const CompareFrom = (emails) => {
     const newArr = emails.slice();
     newArr.sort(compareFrom);
-    console.log(newArr===emails);
+    // console.log(newArr === emails);
+    return newArr;
+  };
+  const CompareTo = (emails) => {
+    const newArr = emails.slice();
+    newArr.sort(compareTo);
+    // console.log(newArr === emails);
+    return newArr;
+  };
+  const CompareSubect = (emails) => {
+    const newArr = emails.slice();
+    newArr.sort(compareSubject);
+    // console.log(newArr === emails);
+    return newArr;
+  };
+  const CompareDate = (emails) => {
+    const newArr = emails.slice();
+    newArr.sort(compareDate);
+    // console.log(newArr === emails);
     return newArr;
   };
 
-  function MessagesList ({value}) {
+  function MessagesList({ value }) {
     let listEmails = value.map((items, index) => (
-      <ol className="EmailOL" key = {Math.random()}>
+      <ol className="EmailOL" key={Math.random()}>
         <li
           className="Email"
           key={Math.random()}
@@ -122,44 +228,42 @@ function App() {
           {items.subject}
         </li>
         <li className="Email" key={Math.random()}>
-          {items.date}
+          {calcDate(items.date)}
         </li>
       </ol>
     ));
     return (
-      <ul key = {Math.random()}>
-        <ol className="EmailTitles" id="emailTitles" key = {Math.random()} >
+      <ul key={Math.random()}>
+        <ol className="EmailTitles" id="emailTitles" key={Math.random()}>
           <li
-            key = {Math.random()}
+            key={Math.random()}
             className="Title"
             id="title"
-            // onClick={() => setEmails(emails.sort(compareFrom))}
-            // onClick={() => setEmails(emails.sort(compareFrom))}
-            onClick={() => setEmails(emailArrReturn(emails))}
+            onClick={() => setEmails(CompareFrom(emails))}
           >
             From
           </li>
           <li
-         key = {Math.random()}
+            key={Math.random()}
             className="Title"
             id="title"
-            onClick={() => setEmails(emails.sort(compareTo))}
+            onClick={() => setEmails(CompareTo(emails))}
           >
             To
           </li>
           <li
-          key = {Math.random()}
+            key={Math.random()}
             className="Title"
             id="title"
-            onClick={() => setEmails(emails.sort(compareSubject))}
+            onClick={() => setEmails(CompareSubect(emails))}
           >
             Subject
           </li>
           <li
-          key = {Math.random()}
+            key={Math.random()}
             className="Title"
             id="title"
-            onClick={() => console.log("Date")}
+            onClick={() => setEmails(CompareDate(emails))}
           >
             Date
           </li>
@@ -167,11 +271,7 @@ function App() {
         {listEmails}
       </ul>
     );
-  };
-
-  useEffect(() => {
-    console.log(emails[1].from)
-  }, [emails]);
+  }
   // Make a list of emails
   if (emails.length > 0) {
     return (
@@ -204,17 +304,17 @@ function App() {
     return (
       <div className="App">
         <div className="Dates">
-          <div className="Calender">
-            <p>Calender</p>
-          </div>
-          {/* Calender end */}
-          <div className="Search">
-            <p>Search</p>
-          </div>
-          {/* Search end */}
-        </div>
-        {/* Dates end */}
-        <div className="Results">
+          <form onSubmit={SearchClick} className="search-form">
+            {/* reaplce input with calander input */}
+            <div>
+              {" "}
+              <Calendar onChange={onChange} value={value} />
+            </div>
+            <button className="Search-Container">
+              <img className="Search" src={Search} alt="Search" type="submit" />
+            </button>
+          </form>
+          {/* Dates end */}
           <p>Results: {resultsLength}mail(s)</p>
         </div>
         {/* Results end */}
